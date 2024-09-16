e27508fb82f3a2905e164f2aa941bcf9dcf6f0bd
MAGIC-USER-NU <lakshitgiri@2005@gmail.com>
2024-09-16 22:16:41 +0530

    posting

--- DAY2/lec2.java
+++ DAY2/lec2.java
@@ -6,15 +6,14 @@ public class lec2 {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=n;i>0;i--){
-            System.out.println(i);
-
+        // for(int i=n;i>0;i--){
+        //     System.out.println(i);      //with for loop
+        // }
+        int i=0;
+        while(i<n){
+            System.out.println(n);         //with while loop
+            n--;
         }
 
-
-
-
-
     }
-
 }
---
+++ DAY2/lec3.java
@@ -0,0 +1,17 @@
+//// WAP to print first N even numbers
+
+package DAY2;
+import java.util.Scanner;
+public class lec3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=2;i<=n;i=i+2){
+            System.out.println(i);
+
+        }
+
+
+
+    }
+}
