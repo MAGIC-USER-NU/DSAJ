660f644f54070f590e17253b8c9f55b667ffd8fb
MAGIC-USER-NU <lakshitgiri@2005@gmail.com>
2024-10-20 13:07:52 +0530

    pushing

--- DAY6/Q1.java
+++ DAY6/Q1.java
@@ -1,13 +1,20 @@
 package DAY6;
-import Math
 
 public class Q1 {
+    static int maxSubArray(int[] nums) {
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++){
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+            if(currSum<0) 
+                currSum=0;
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
-        
-
-
-
-
-
+        int[] arr = {1,2,3,4,5};
+        int x=maxSubArray(arr);
+        System.out.println(x);
     }
 }
